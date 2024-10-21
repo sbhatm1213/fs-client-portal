94da891c88529c89ecbe5d0dcec9ffe4f2f75235
Sowjanya R Bhat <sbhatm1213@gmail.com>
2024-10-21 20:33:32 +0530

    redirect uri updated

--- src/services/AuthProvider.js
+++ src/services/AuthProvider.js
@@ -38,8 +38,8 @@ export const AuthProvider = ({ children }) => {
 
   const signInWithAzure = async () => {
           await account.createOAuth2Session('microsoft',
-                        'https://main.d3n19dnhhpdakz.amplifyapp.com/v1/account/sessions/oauth2/callback/azure',
-                        'https://main.d3n19dnhhpdakz.amplifyapp.com/v1/account/sessions/oauth2/callback/azure');
+                        'https://main.d3n19dnhhpdakz.amplifyapp.com/v1/account/sessions/oauth2/callback/microsoft',
+                        'https://main.d3n19dnhhpdakz.amplifyapp.com/v1/account/sessions/oauth2/callback/microsoft');
   };
 
   const signOut = async () => {
